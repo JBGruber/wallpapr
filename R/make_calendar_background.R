d41d7b330640df1dac2ec21d8ae1c18f708deeb3
JBGruber <j.gruber.1@research.gla.ac.uk>
2020-11-07 19:46:06 +0100

    Disentangled data and plotting

--- R/make_calendar_background.R
+++ R/make_calendar_background.R
@@ -48,6 +48,79 @@ make_calendar_background <- function(img,
                                      locale = NULL) {
   imgage <- image_read(img)
 
+  plot_data <- calender_data(
+    month = month,
+    headline_factor = headline_factor,
+    locale = locale,
+    start_monday = start_monday
+  )
+
+  plot_data$size <- 1
+  plot_data$size[1] <- headline_factor
+
+  dpi <- text_size * 100
+
+  if (isTRUE(resolution == "auto")) {
+    width <- image_info(imgage)[[2]]
+    height <- image_info(imgage)[[3]]
+  } else if (length(resolution) >= 2) {
+    width <- resolution[1]
+    height <- resolution[2]
+    if (scale) {
+      scl <- image_scale(imgage, geometry_area(width = width))
+      if (image_info(scl)[[3]] < height) {
+        scl <- image_scale(imgage, geometry_area(height = height))
+      }
+      imgage <- scl
+    }
+    imgage <- image_crop(imgage, geometry_area(width = width, height = height))
+  }
+
+  plot <- ggplot(
+    plot_data,
+    aes_string(
+      x = "day",
+      y = "week",
+      label = "text",
+      size = "size"
+    )
+  ) +
+    annotation_custom(grid::rasterGrob(imgage)) +
+    geom_text(colour = colour, family = family, show.legend = FALSE) +
+    scale_size_identity() +
+    scale_y_reverse(expand = c(1, -1)) +
+    scale_x_discrete(expand = c(1, 1)) +
+    theme_void() +
+    theme(panel.background = element_rect(fill = fill))
+
+  ggsave(
+    filename = filename,
+    dpi = dpi,
+    width = width / dpi,
+    height = height / dpi,
+    units = "in",
+    plot = plot
+  )
+  message("wallpaper saved as ", filename)
+}
+
+
+#' Make calendar data
+#'
+#' Make the calendar data for plotting.
+#'
+#' @param month either a date or name of a month in the current locale.
+#' @param headline_factor the factor by which the name of the month is larger
+#'   then the remaining text (2 means twice the size).
+#' @param start_monday If TRUE the week starts on Monday, otherwise starts on
+#'   Sunday.
+#' @param locale provide locale if you want to use non English names for months
+#'   and days.
+calender_data <- function(month,
+                          headline_factor,
+                          start_monday,
+                          locale) {
+
   if (is.character(month)) {
     month <- as.Date(
       stri_datetime_parse(
@@ -132,56 +205,10 @@ make_calendar_background <- function(img,
   )
 
   plot_data <- rbind(dat, dat2,
-    stringsAsFactors = FALSE
+                     stringsAsFactors = FALSE
   )
 
-  plot_data$size <- 1
-  plot_data$size[1] <- headline_factor
-
-  dpi <- text_size * 100
-
-  if (isTRUE(resolution == "auto")) {
-    width <- image_info(imgage)[[2]]
-    height <- image_info(imgage)[[3]]
-  } else if (length(resolution) >= 2) {
-    width <- resolution[1]
-    height <- resolution[2]
-    if (scale) {
-      scl <- image_scale(imgage, geometry_area(width = width))
-      if (image_info(scl)[[3]] < height) {
-        scl <- image_scale(imgage, geometry_area(height = height))
-      }
-      imgage <- scl
-    }
-    imgage <- image_crop(imgage, geometry_area(width = width, height = height))
-  }
-
-  plot <- ggplot(
-    plot_data,
-    aes_string(
-      x = "day",
-      y = "week",
-      label = "text",
-      size = "size"
-    )
-  ) +
-    annotation_custom(grid::rasterGrob(imgage)) +
-    geom_text(colour = colour, family = family, show.legend = FALSE) +
-    scale_size_identity() +
-    scale_y_reverse(expand = c(1, -1)) +
-    scale_x_discrete(expand = c(1, 1)) +
-    theme_void() +
-    theme(panel.background = element_rect(fill = fill))
-
-  ggsave(
-    filename = filename,
-    dpi = dpi,
-    width = width / dpi,
-    height = height / dpi,
-    units = "in",
-    plot = plot
-  )
-  message("wallpaper saved as ", filename)
+  return(plot_data)
 }
 
 
